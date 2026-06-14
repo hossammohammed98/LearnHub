431e1e918523fd0aa21271f507d0a10e586246be
ahmed hassan <hasnm8722@gmail.cm>
2026-06-14 20:02:19 +0300

    Add BackEnd Folder and The base Structure

---
+++ Back-end/src/shared/core/BaseRepository.js
@@ -0,0 +1,27 @@
+class BaseRepository {
+    constructor(model) {
+        this.model = model;
+    }
+    async getAll(filter = '', populateOption = null) {
+        const data = await this.model.find().populate(populateOption)
+        return data;
+
+    }
+    async getById(id, populateOption = null) {
+        const data = await this.model.findById(id).populate(populateOption);
+        return data;
+    }
+    async create(newModel) {
+        return await this.model.create(newModel);
+    }
+    async update(id, updateModel) {
+        const data = await this.model.findByIdAndUpdate(id, updateModel);
+        return data;
+    }
+    async delete(id) {
+        const data = await this.model.findByIdAndDelete(id);
+        return data;
+    }
+
+}
+module.exports = BaseRepository;
---
+++ Back-end/src/shared/core/BaseService.js
@@ -0,0 +1,21 @@
+class BaseService{
+    constructor(repository){
+        this.repository=repository;
+    }
+    async getAll(filter='',populateOption=null){
+        return await this.repository.getAll(filter,populateOption);
+    }
+    async getById(id,populateOption=null){
+        return await this.repository.getById(id,populateOption);
+    }
+    async create(newModel){
+        return await this.repository.create(newModel);
+    }
+    async update(id,updateModel){
+        return await this.repository.update(id,updateModel);
+    }
+    async delete(id){
+        return await this.repository.delete(id);
+    }
+}
+module.exports=BaseService;
