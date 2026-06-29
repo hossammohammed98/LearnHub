40f5a1fdb2af6974386dffaf531c511aa25199a2
his0ka7 <mnassim799@gmail.com>
2026-06-29 14:01:08 +0300

    loginpage back

--- client/src/app/(auth)/register/page.tsx
+++ client/src/app/(auth)/register/page.tsx
@@ -111,7 +111,7 @@ export default function RegisterPage() {
         <p className="text-sm text-slate-400 mt-1">
           اختر نوع حسابك للبدء في المنصة
 
-        </p> */}
+        </p> 
 
       </div>
 
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -5,7 +5,7 @@ import StepsSection from "@/features/landingpage/component/StepsSection";
 import PricingSection from "@/features/landingpage/component/PricingSection";
 import CtaBanner from "@/features/landingpage/component/CtaBanner";
 import Footer from "@/features/landingpage/component/Footer";
-import LandingPage from "./landingpage/page";
+import LandingPae from "./landingpage/page";
 
 // مكونات الشات
 import ConversationItem from "@/features/chat/components/ConversationItem";
@@ -15,13 +15,14 @@ import ConversationItem from "@/features/chat/components/ConversationItem";
 import { ActivityCard } from "@/features/parent/components/ActivityCard";
 import { FamilyPerformance } from "@/features/parent/components/FamilyPerformance";
 import StatCard from "@/features/parent/components/StateCad";
+import { redirect } from "next/navigation";
 
 
 export default function HomePage() {
   // إذا كنت تريد تحويل المستخدم مباشرة لصفحة تسجيل الدخول، فك التعليق عن السطر التالي:
   redirect("/register");
 
-export default function LandingPage() {
+
   return (
     <>   
      {/* <StudentDashboard></StudentDashboard> */}
